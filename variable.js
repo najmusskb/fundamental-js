46f2fac6e23badc67bc20897a201b9d1def84b83
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-31 20:20:01 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,16 @@
+// number
+
+var watchPrice = 200;
+watchPrice = 225;
+
+
+// String
+var userName = 'sakib';
+
+
+// boolean 
+
+var isexpensive = true;
+// integer :1 2 3 4 5 6 7 8 9 145
+// float 2.754   5.467 3.14
+
